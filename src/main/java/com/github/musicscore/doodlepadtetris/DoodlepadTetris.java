7571c7e9f782c5dfe58acc64e5967655d9603137
MusicScore <16635607+MusicScore@users.noreply.github.com>
2018-12-04 22:36:13 -0500

    Initial commit

---
+++ src/main/java/com/github/musicscore/doodlepadtetris/DoodlepadTetris.java
@@ -0,0 +1,11 @@
+package com.github.musicscore.doodlepadtetris;
+
+import doodlepad.*;
+
+public class DoodlepadTetris {
+
+    public static void main(String[] args) {
+        Pad pad = new Pad(100, 200);
+    }
+
+}
